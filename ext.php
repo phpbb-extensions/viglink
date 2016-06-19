a9a309adf21cc40503c0ba21d8752a4bfe61676f
Matt Friedman <maf675@gmail.com>
2016-06-19 16:53:05 -0700

    Update min. phpbb requirements

--- ext.php
+++ ext.php
@@ -20,14 +20,14 @@ class ext extends \phpbb\extension\base
 	 * The current phpBB version should meet or exceed
 	 * the minimum version required by this extension:
 	 *
-	 * Requires phpBB 3.1.2 or greater
+	 * Requires phpBB 3.2.0-b1 or greater
 	 *
 	 * @return bool
 	 * @access public
 	 */
 	public function is_enableable()
 	{
-		return phpbb_version_compare(PHPBB_VERSION, '3.1.2', '>=');
+		return phpbb_version_compare(PHPBB_VERSION, '3.2.0-b1', '>=');
 	}
 
 	/**
